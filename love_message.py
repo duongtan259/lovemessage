45605d4ed099b853afa1376d70365032a7998515
duongtan259 <duongphuctan259@gmail.com>
2024-10-02 22:39:06 +0300

    push

---
+++ love_message.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# Set the page title and emoji icon
+st.set_page_config(page_title="Surprise!", page_icon="❤️")
+
+# Display the love message
+st.title("❤️ I Love You ❤️")
+
+# Add animated hearts using emojis
+st.write("You're the best thing that ever happened to me! Anh Yêu Em (Nguyễn Thị Phương Anh) 💖💖💖")
+
+# Add more hearts
+st.markdown("💘💘💘💖💖💖💓💓💓")
+
+# Optionally, add an image of hearts or animation
+st.image("https://media.giphy.com/media/l0HUqsz2jdQYElRm0/giphy.gif", use_column_width=True)
